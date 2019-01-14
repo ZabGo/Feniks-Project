ccf523e0f61df231726b300ccecc98d856ecfe6d
smelge <traildogfilms@gmail.com>
2019-01-14 15:03:14 +0000

    Added Basic Routes

---
+++ feniksproject/src/components/pages/NewClient.js
@@ -0,0 +1,9 @@
+import React,{Fragment} from 'react';
+import {Link} from 'react-router-dom';
+
+const NewClient=()=>(
+  <Fragment>
+    New Client Page
+  </Fragment>
+)
+export default NewClient;
